da0d83e864e5a067c3aed16e2cea4eaca7800600
Liwencan <354621693@qq.com>
2022-08-08 15:51:42 +0800

    hello world

---
+++ test-app/src/main/java/com/leemanshow/testapp/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.leemanshow.testapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author leewencan
+ * @date 2022/8/8 15:50
+ */
+@RestController
+public class HelloController {
+    @GetMapping("/manshow/hi")
+    public String aa(){
+        return "hello you fuuker";
+    }
+}
