3eeb7fcb909e4b984811b0608f6d84772fa29429
Liwencan <354621693@qq.com>
2022-09-05 21:22:28 +0800

    fabric8

--- test-app/src/main/java/com/leemanshow/testapp/controller/HelloController.java
+++ test-app/src/main/java/com/leemanshow/testapp/controller/HelloController.java
@@ -1,6 +1,5 @@
 package com.leemanshow.testapp.controller;
 
-import com.alibaba.fastjson.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,11 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/manshow/hi")
     public String aa(){
-        return "hello !!!!!！！！!";
+        return "hello !";
     }
     @PostMapping("/manshow/{ss}")
     public String aa1(@RequestBody SS ss){
         System.out.println(ss.b);
-        return "测测测 !!!!!！！！!";
+        return "!";
     }
 }
